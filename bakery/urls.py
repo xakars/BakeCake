1c73585c3508bb52fed475f4ea75c29c3d672495
Ars <xakars02@gmail.com>
2023-07-26 14:40:42 +0800

    connect front

---
+++ bakery/urls.py
@@ -0,0 +1,11 @@
+from . import views
+from django.urls import path
+
+
+app_name = "bakery"
+
+
+urlpatterns = [
+    path('', views.view_index, name='main_page'),
+    path('lk/', views.view_lk, name='lk_page')
+]
