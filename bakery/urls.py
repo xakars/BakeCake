00ebb4a94a523f668ecd2ad17fbe4a44b0f28a48
ars <xakars02@gmail.com>
2023-07-28 14:06:45 +0800

    fix models

--- bakery/urls.py
+++ bakery/urls.py
@@ -1,6 +1,6 @@
 from . import views
 from django.urls import path
-from .views import order_cake
+
 
 app_name = "bakery"
 
@@ -8,5 +8,4 @@ app_name = "bakery"
 urlpatterns = [
     path('', views.view_index, name='main_page'),
     path('lk/', views.view_lk, name='lk_page'),
-    path('order_cake/', order_cake, name='order_cake'),
 ]
